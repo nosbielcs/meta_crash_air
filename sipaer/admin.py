ef704ce56df0a77c628eaca29bc65b3fa2a63d44
Cleibson Almeida <nosbielcs@bol.com.br>
2014-07-18 12:05:47 -0300

    documento admin

--- sipaer/admin.py
+++ sipaer/admin.py
@@ -1,5 +1,9 @@
+# -*- encoding: utf-8 -*-
 from django.contrib import admin
-from sipaer.models import Ocorrencia, Aeronave, Fator, Lesao, Tripulacao, AeronaveDetalhe, FatorContribuicao
+from notificacao.models import Ocorrencia, Aeronave, Fator, Lesao, Tripulacao, AeronaveDetalhe, FatorContribuicao, Pais, Uf, Cidade, Aerodromo, Violacao, ViolacaoDetalhe, Documento
+
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ('cidade',)
 
 class AeronaveAdmin(admin.ModelAdmin):
     list_display = ('matricula', 'equipamento', 'fabricante', 'modelo', 'categoria')
@@ -8,18 +12,34 @@ class AeronaveAdmin(admin.ModelAdmin):
     ordering = ('categoria',)
     #fields = ('matricula', 'equipamento', 'fabricante', 'modelo', 'categoria')
 
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ('documento','status_documento')
+
 class FatorAdmin(admin.ModelAdmin):
     list_display = ('area','nome_fator')
     search_fields = ('nome_fator', 'area')
     ordering = ('area',)
 
 class FatorContribuicaoAdmin(admin.ModelAdmin):
-    list_display = ('nivel_contribuicao',)
-    ordering = ('nivel_contribuicao',)
+    list_display = ('latente','nivel_contribuicao',)
+    ordering = ('latente','nivel_contribuicao',)
 
 class FatorContribuicaoInline(admin.TabularInline):
     model = FatorContribuicao
-    extra = 1
+    extra = 0
+
+class ViolacaoAdmin(admin.ModelAdmin):
+    list_display = ('area','tipo')
+    search_fields = ('tipo', 'area')
+    ordering = ('area',)
+
+class ViolacaoDetalheAdmin(admin.ModelAdmin):
+    list_display = ('violacao','detalhe',)
+    ordering = ('violacao','detalhe',)
+
+class ViolacaoDetalheInline(admin.TabularInline):
+    model = ViolacaoDetalhe
+    extra = 0
 
 class TripulacaoAdmin(admin.ModelAdmin):
     list_display = ('codigo_anac','funcao')
@@ -27,7 +47,7 @@ class TripulacaoAdmin(admin.ModelAdmin):
 
 class TripulacaoInline(admin.TabularInline):
     model = Tripulacao
-    extra = 1
+    extra = 0
 
 class LesaoAdmin(admin.ModelAdmin):
     list_display = ('tipo_lesao','quantidade')
@@ -36,7 +56,7 @@ class LesaoAdmin(admin.ModelAdmin):
 
 class LesaoInline(admin.TabularInline):
     model = Lesao
-    extra = 1
+    extra = 0
 
 class AeronaveDetalheAdmin(admin.ModelAdmin):
     list_display = ('aeronave','operador', 'fase_voo')
@@ -45,9 +65,13 @@ class AeronaveDetalheAdmin(admin.ModelAdmin):
 
 class AeronaveDetalheInline(admin.StackedInline):
     model = AeronaveDetalhe
-    extra = 1
+    extra = 0
     inlines = [LesaoInline,TripulacaoInline]
     #readonly_fields = ('changeform_link', )
+
+class DocumentoInline(admin.TabularInline):
+    model = Documento
+    extra = 0
 
 class OcorrenciaAdmin(admin.ModelAdmin):
     list_display = ('classificacao','tipo','aerodromo','dia')
@@ -56,13 +80,22 @@ class OcorrenciaAdmin(admin.ModelAdmin):
     #ordering = ('dia',)
     #date_hierarchy = 'dia'
     #ERRO#raw_id_fields = ('lesao',)
-    inlines = [AeronaveDetalheInline, FatorContribuicaoInline,]
+    inlines = [DocumentoInline,AeronaveDetalheInline, FatorContribuicaoInline, ViolacaoDetalheInline,]
 
 # Register your models here.
+admin.site.register(Pais)
+admin.site.register(Uf)
+admin.site.register(Cidade, CidadeAdmin)
+admin.site.register(Aerodromo)
 admin.site.register(Aeronave, AeronaveAdmin)
+admin.site.register(Documento, DocumentoAdmin)
 admin.site.register(Fator, FatorAdmin)
+admin.site.register(Violacao, ViolacaoAdmin)
 admin.site.register(Lesao, LesaoAdmin)
 admin.site.register(Tripulacao, TripulacaoAdmin)
 admin.site.register(AeronaveDetalhe, AeronaveDetalheAdmin)
 admin.site.register(FatorContribuicao, FatorContribuicaoAdmin)
+admin.site.register(ViolacaoDetalhe, ViolacaoDetalheAdmin)
 admin.site.register(Ocorrencia, OcorrenciaAdmin)
+
+#FIM ADMIN
